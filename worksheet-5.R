45a39b9467968233b5db535da36d7af9ae4ae688
Andres Garcia <andresgarcia@Andress-MacBook-Pro-2.local>
2021-04-05 15:46:09 -0400

    changed the map in leaflethtml

--- worksheet-5.R
+++ worksheet-5.R
@@ -4,10 +4,16 @@
 library(htmlwidgets)
 library(leaflet)
 
-# Crate a map with point and tiles
-rand_lng = function(n = 10) rnorm(n, -93.65, .01)
-rand_lat = function(n = 10) rnorm(n, 42.0285, .01)
-map = leaflet() %>% addTiles() %>% addCircles(rand_lng(50), rand_lat(50), radius = runif(50, 10, 200))
+# Crate a map 
+map <- leaflet() %>%
+  addTiles() %>%
+  setView(lng = -76.505206, lat = 38.9767231, zoom = 5) %>%
+  addWMSTiles(
+    "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
+    layers = "nexrad-n0r-900913", 
+    options = WMSTileOptions(format = "image/png", transparent = TRUE),
+    attribution = "Weather data © 2012 IEM Nexrad"
+  )
 
 # Save map to html
 saveWidget(map, file="map.html")
